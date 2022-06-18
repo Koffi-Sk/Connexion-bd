00f1f126df93ea6bef285e0970881356b83a666b
Koffi-Sk <kofdzakou1411@gmail.com>
2022-06-18 11:09:21 -0400

    Connexion à la bd

--- index.php
+++ index.php
@@ -9,7 +9,52 @@
 </head>
 
 <body>
+    <?php
+        // Constantes d'environnement 
+        define("DBHOST", "localhost");
+        define("DBUSER", "root");
+        define("DBPASS", "mysql");
+        define("DBNAME", "api_php");
 
+        // DSN de connexion
+        $dsn = "mysql:dbname=" .DBNAME.";host=".DBHOST;
+
+        // On va se connecter à la base
+        try {
+            // On instancie PDO
+            $db = new PDO($dsn, DBUSER, DBPASS);
+
+            // On s'assure d'envoyer les données en UTF8
+            $db->exec("SET NAMES utf8");
+
+            // On définit le mode de "fetch" par défaut
+            $db->setAttribute
+            (PDO::ATTR_DEFAULT_FETCH_MODE,
+            PDO::FETCH_ASSOC);
+
+        }catch(PDOException $e){
+            die("Error".$e->getMessage()); 
+
+        }
+
+        // Ici on est connecté à la bd
+        $sql = "SELECT SELECT f.id, f.libelle, f.description, f.image, c.libelle as 'categorie'
+        from formation f inner join categorie c on f.categorie_id = c.id";
+
+        // On exécute directement la requête
+        //$req = $db->query($sql);
+
+        $stmt = $db->prepare($sql);
+        $stmt->execute();
+
+        // On récupère les données en tableau (fetch ou fetchAll)
+
+        $formations = $stmt->fetchAll();
+
+        echo "<pre>";
+        var_dump($formations);
+        echo "</pre>";
+    ?>
 </body>
 
 </html>
